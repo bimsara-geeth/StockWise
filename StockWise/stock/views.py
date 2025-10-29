2ece19e44e4cd2aeebf057e346411aef1e79eea0
bimsara-geeth <bimsarageeth2005@gmail.com>
2025-10-30 02:23:33 +0530

    stock details compleate

--- StockWise/stock/views.py
+++ StockWise/stock/views.py
@@ -1,19 +1,88 @@
 from django.shortcuts import render
 import requests
 
-base_url = "https://www.cse.lk/api/"
-endpoint = "tradeSummary"
+def get_symbols():
+    base_url = "https://www.cse.lk/api/"
+    endpoint = "tradeSummary"
 
-response = requests.post(base_url + endpoint)
-file = response.json()
+    response = requests.post(base_url + endpoint)
+    file = response.json()
 
-trade_summary = (
-    file.get("reqTradeSummery")
-)
+    trade_summary = (
+        file.get("reqTradeSummery")
+    )
 
-symbols = []
+    symbols = []
 
-for item in trade_summary:
-    symbols.append(item["symbol"])
+    for item in trade_summary:
+        symbols.append(item["symbol"])
 
-print(symbols)
+    return symbols
+
+def get_price(symbol):
+    base_url = "https://www.cse.lk/api/"
+    endpoint = "tradeSummary"
+
+    response = requests.post(base_url + endpoint)
+    file = response.json()
+
+    trade_summary = (
+        file.get("reqTradeSummery")
+    )
+
+    for item in trade_summary:
+        if item["symbol"] == symbol:
+            return item["price"]
+
+    return None
+
+def get_name(symbol):
+    base_url = "https://www.cse.lk/api/"
+    endpoint = "tradeSummary"
+
+    response = requests.post(base_url + endpoint)
+    file = response.json()
+
+    trade_summary = (
+        file.get("reqTradeSummery")
+    )
+
+    for item in trade_summary:
+        if item["symbol"] == symbol:
+            return item["name"]
+
+    return None
+
+def get_change(symbol):
+    base_url = "https://www.cse.lk/api/"
+    endpoint = "tradeSummary"
+
+    response = requests.post(base_url + endpoint)
+    file = response.json()
+
+    trade_summary = (
+        file.get("reqTradeSummery")
+    )
+
+    for item in trade_summary:
+        if item["symbol"] == symbol:
+            return item["change"]
+
+    return None
+
+def get_percentage_change(symbol):
+    base_url = "https://www.cse.lk/api/"
+    endpoint = "tradeSummary"
+
+    response = requests.post(base_url + endpoint)
+    file = response.json()
+
+    trade_summary = (
+        file.get("reqTradeSummery")
+    )
+
+    for item in trade_summary:
+        if item["symbol"] == symbol:
+            return item["percentageChange"]
+
+    return None
